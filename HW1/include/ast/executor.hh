4483479d64afdd34d27664c2b8dcd9ed1475bab3
Zecyel <i@zecyel.xyz>
2026-03-09 18:16:43 +0800

    chore: moved class definition to .hh

--- HW1/include/ast/executor.hh
+++ HW1/include/ast/executor.hh
@@ -4,10 +4,33 @@
 #include "ASTheader.hh"
 #include "FDMJAST.hh"
 #include <map>
+#include <string>
 
 using namespace std;
 using namespace fdmj;
 
 int execute(Program *root);
+
+class ExecutorVisitor : public ASTVisitor {
+public:
+  map<string, int> table;
+  int returnValue = 0;
+
+  void visit(Program *node) override;
+  void visit(MainMethod *node) override;
+  void visit(Assign *node) override;
+  void visit(Return *node) override;
+  void visit(BinaryOp *node) override;
+  void visit(UnaryOp *node) override;
+  void visit(IdExp *node) override;
+  void visit(OpExp *node) override;
+  void visit(IntExp *node) override;
+
+  int evalExpression(Exp *node);
+  int getVariableValue(IdExp *id);
+  int evalUnary(UnaryOp *node);
+  int evalBinary(BinaryOp *node);
+  void reportUndefined(IdExp *id);
+};
 
 #endif
